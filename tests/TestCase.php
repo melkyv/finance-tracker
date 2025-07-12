cf2e00112f5726ccb6743bfc1edf87dfb1612b38
melkyv <melkytam17@gmail.com>
2025-07-12 15:49:53 -0300

    fix: add kafka mock

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Junges\Kafka\Facades\Kafka;
+use Mockery;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock Kafka facade
+        $messageProducerMock = Mockery::mock(\Junges\Kafka\Contracts\MessageProducer::class);
+        $messageProducerMock->shouldReceive('onTopic')->andReturnSelf();
+        $messageProducerMock->shouldReceive('withMessage')->andReturnSelf();
+        $messageProducerMock->shouldReceive('send')->andReturn(true);
+
+        Kafka::shouldReceive('publish')->andReturn($messageProducerMock);
+    }
 }
