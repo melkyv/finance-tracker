8c75352b2396101318d4a8de5b889ed2efb00ff7
melkyv <melkytam17@gmail.com>
2025-07-14 19:20:02 -0300

    feat: add laravel sanctum and API routes

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AccountController;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\CategoryController;
+use App\Http\Controllers\Api\V1\PasswordResetController;
+use App\Http\Controllers\Api\V1\ProfileController;
+use App\Http\Controllers\Api\V1\ReportController;
+use App\Http\Controllers\Api\V1\TransactionController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/token', [AuthController::class, 'token']);
+
+    Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::post('/reset-password', [PasswordResetController::class, 'reset'])->name('password.update');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+        Route::get('/user', [ProfileController::class, 'show']);
+        Route::put('/user', [ProfileController::class, 'update']);
+        Route::put('/user/password', [ProfileController::class, 'updatePassword']);
+
+        Route::apiResource('accounts', AccountController::class);
+        Route::apiResource('categories', CategoryController::class);
+        Route::apiResource('transactions', TransactionController::class);
+        Route::apiResource('reports', ReportController::class)->except(['update']);
+    });
+});
